8f8e500114a35a1a1e159ab5f44aa7072d745667
Arya Solhi <aryasolhi@gmail.com>
2015-08-15 12:07:52 -0700

    Set up dependencies in index file and made the angular app

---
+++ app/app.js
@@ -0,0 +1,17 @@
+(function(){
+    
+    'use strict';
+    
+    var anonimo = angular.module('anonimo', []);
+    
+    anonimo.controller('AnonimoController',["", function(){
+        var anonimo = this;
+        
+        anonimo.yolo = "thingsworksortof"
+        
+    }])
+    
+    
+    
+    
+})()
