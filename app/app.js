12535ba627712fc06adb75cb98031ace674ac130
Arya Solhi <aryasolhi@gmail.com>
2015-08-15 12:34:14 -0700

    Possibly broke everything or permissions.  Let's see.

--- app/app.js
+++ app/app.js
@@ -1,17 +1,26 @@
 (function(){
-    
     'use strict';
-    
-    var anonimo = angular.module('anonimo', []);
-    
-    anonimo.controller('AnonimoController',["", function(){
-        var anonimo = this;
-        
-        anonimo.yolo = "thingsworksortof"
-        
-    }])
-    
-    
-    
-    
+
+    var anonimo = angular.module("anonimo", [
+        'ngAnimate',
+        'ngRoute',
+        'landingpage'
+    ]);
+
+    anonimo.config(['$routeProvider','$locationProvider',
+      function($routeProvider,$locationProvider) {
+        $locationProvider.html5Mode(true).hashPrefix('!');
+        $routeProvider.
+          when('/', {
+            templateUrl: 'landingpage/landingpage.html',
+            controller: 'LandingPageController as lpCtrl'
+          }).
+          when('/404', {
+            templateUrl: '404/404.html',
+            controller: 'SearchController as search'
+          }).
+          otherwise({
+            redirectTo: '/404'
+          });
+      }]);
 })()
---
+++ app/landingpage/landingpagecontroller.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+    
+    var landingpage = angular.module('landingpage', [])
+    
+    landingpage.controller('LandingPageController', function(){
+        var landingpage = this;
+    })
+    
+})()
