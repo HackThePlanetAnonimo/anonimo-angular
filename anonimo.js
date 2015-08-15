cc923df86a37f71cbdbb5914c4145dafaafe14f8
Jason Lin <uscjlin@gmail.com>
2015-08-15 06:54:45 -0700

    basic Angular boilerplates

---
+++ anonimo.js
@@ -0,0 +1,17 @@
+function anonimoCtrl($scope) {
+
+    $scope.totalAnonimos = 4;
+}
+
+// .config(['$routeProvider', function($routeProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: '../static/all_questions.html',
+//             controller: 'allQuestions'
+//         })
+//         .when('/secondPage', {
+//             templateUrl: '../static/individual.html',
+//             controller: 'Individual'
+//         })
+//         .otherwise({ redirectTo: '/' });
+// }])
