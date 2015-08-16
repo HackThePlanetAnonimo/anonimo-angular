fa459a7dcf6b55669eff4604fc7e25290156266b
Arya Solhi <aryasolhi@gmail.com>
2015-08-16 00:43:47 -0700

    stuff

--- app/landingpage/landingpagecontroller.js
+++ app/landingpage/landingpagecontroller.js
@@ -6,26 +6,44 @@
     landingpage.controller('LandingPageController', ["$scope", "$http", function($scope, $http){
         $scope.signupstatus = false;
         $scope.showButtons = true;
-        $scope.setSignUpMode = function(mode){
+        $scope.showSignIn = true;
+        
+        $scope.setMode = function(mode){
             console.log("clicked");
             $scope.showButtons = false;
             $scope.isProf = mode;
-        }
+        }      
+
         $scope.signUp = function(){
             if ($scope.isProf){
-            $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_up', {
-                "Email": $scope.email,
-                "Password": $scope.password,
-                "Name": $scope.name
-            }).then(function(res){
-                console.log("shit worked: "+res);
-            }, function(res){
-                console.log(res);
+                $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_up', {
+                    "Email": $scope.email,
+                    "Password": $scope.password,
+                    "Name": $scope.name
+                }).then(function(res){
+                    if (res.data.success == false) {
+                        alert("Email already exists");
+                    }
+                    console.log(res);
+                }, function(res){
+                    console.log(res);
 
-            })
+                });
             
             } else {
-                
+                $http.post('https://vast-earth-4742.herokuapp.com/student_sign_up', {
+                    "Email": $scope.email,
+                    "Password": $scope.password,
+                    "Name": $scope.name
+                }).then(function(res){
+                    if (res.data.success == false) {
+                        alert("Email already exists");
+                    }
+                    console.log(res);
+                }, function(res){
+                    console.log(res);
+
+                });
             }
         }
     }]);
