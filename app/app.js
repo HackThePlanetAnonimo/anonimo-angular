bf0a68d5955c20450ee14099ac123cfe3c794801
Aditya Sridhar <aditya123ss@gmail.com>
2015-11-09 00:28:28 -0800

    New UI for sign in

--- app/app.js
+++ app/app.js
@@ -15,27 +15,27 @@
       function($routeProvider,$locationProvider) {
         $locationProvider.html5Mode(true).hashPrefix('!');
         $routeProvider.
-          when('/', {
-            templateUrl: 'app/landingpage/landingpage.html',
-            controller: 'LandingPageController as lpCtrl'
-          }).
-          when('/account', {
-            templateUrl: 'app/account/account.html',
-            controller: 'AccountController as account'
-          }).
-          when('/lectureroom', {
-            templateUrl: 'app/lectureroom/lectureroom.html',
-            controller: 'LectureroomController as lectureroom'
-          }).
-          when('/mylectures', {
-            templateUrl: 'app/mylectures/mylectures.html',
-            controller: 'MylecturesController as mylectures'
-          }).
-          when('/404', {
-            templateUrl: '404/404.html'
-          }).
-          otherwise({
-            redirectTo: '/404'
-          });
+            when('/', {
+                templateUrl: 'app/landingpage/landingpage.html',
+                controller: 'LandingPageController as lpCtrl'
+            }).
+            when('/account', {
+                templateUrl: 'app/account/account.html',
+                controller: 'AccountController as account'
+            }).
+            when('/lectureroom', {
+                templateUrl: 'app/lectureroom/lectureroom.html',
+                controller: 'LectureroomController as lectureroom'
+            }).
+            when('/mylectures', {
+                templateUrl: 'app/mylectures/mylectures.html',
+                controller: 'MylecturesController as mylectures'
+            }).
+            when('/404', {
+                templateUrl: '404/404.html'
+            }).
+            otherwise({
+                redirectTo: '/404'
+            });
       }]);
 })()
--- app/landingpage/landingpagecontroller.js
+++ app/landingpage/landingpagecontroller.js
@@ -7,7 +7,9 @@
         $scope.signupstatus = false;
         $scope.showButtons = true;
         $scope.showSignIn = true;
-        
+
+        $scope.userType = 'student';
+
         $scope.setMode = function(mode){
             console.log("clicked");
             $scope.showButtons = false;
@@ -15,90 +17,92 @@
         }      
 
         $scope.signUp = function() {
+            if ($scope.userType === 'professor'){
+                alert ('professor signing up');
+                // $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_up', {
+                //     "Email": $scope.email,
+                //     "Password": $scope.password,
+                //     "Name": $scope.name
+                // }).then(function(res){
+                //     if (res.data.success == false) {
+                //         alert("Email already exists");
+                //     }
+                //     else {
+                //     console.log(res);
+                //     var id = res.data.Professor_id;
+                //     // get id and pass it
 
-            if ($scope.isProf){
-                $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_up', {
-                    "Email": $scope.email,
-                    "Password": $scope.password,
-                    "Name": $scope.name
-                }).then(function(res){
-                    if (res.data.success == false) {
-                        alert("Email already exists");
-                    }
-                    else {
-                    console.log(res);
-                    var id = res.data.Professor_id;
-                    // get id and pass it
+                //     $location.url("/account/?type='professor'&id="+id);
+                //     }
+                // }, function(res){
+                //     console.log(res);
 
-                    $location.url("/account/?type='professor'&id="+id);
-                    }
-                }, function(res){
-                    console.log(res);
-
-                });
+                // });
             
             } else {
-                $http.post('https://vast-earth-4742.herokuapp.com/student_sign_up', {
-                    "Email": $scope.email,
-                    "Password": $scope.password,
-                    "Name": $scope.name
-                }).then(function(res){
-                    if (res.data.success == false) {
-                        alert("Email already exists");
-                    } else {
-                        var id = res.data.Student_id;
-                        $location.url("/mylectures/?type='student'&id="+id);
-                    }
-                    console.log(res);
-                }, function(res){
-                    console.log(res);
+                alert ('student signing up');
+                // $http.post('https://vast-earth-4742.herokuapp.com/student_sign_up', {
+                //     "Email": $scope.email,
+                //     "Password": $scope.password,
+                //     "Name": $scope.name
+                // }).then(function(res){
+                //     if (res.data.success == false) {
+                //         alert("Email already exists");
+                //     } else {
+                //         var id = res.data.Student_id;
+                //         $location.url("/mylectures/?type='student'&id="+id);
+                //     }
+                //     console.log(res);
+                // }, function(res){
+                //     console.log(res);
 
-                });
+                // });
             }
         }
 
         $scope.signIn = function () {
             console.log("signIn");
-            if ($scope.isProf) {
-                $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_in', {
-                    "Email": $scope.email,
-                    "Password": $scope.password
-                }).then(function(res){
-                    console.log(res);
-                    if (res.data.success == false) {
-                        alert("Invalid credentials");
-                    }
-                    else {
-                    console.log(res);
-                    var id = res.data.Professor_id;
-                    // get id and pass it
 
-                    $location.url("/account/?type='professor'&id="+id);
-                    }
-                }, function(res){
-                    console.log(res);
+            if ($scope.userType === 'professor') {
+                // $http.post('https://vast-earth-4742.herokuapp.com/professor_sign_in', {
+                //     "Email": $scope.email,
+                //     "Password": $scope.password
+                // }).then(function(res){
+                //     console.log(res);
+                //     if (res.data.success == false) {
+                //         alert("Invalid credentials");
+                //     }
+                //     else {
+                //     console.log(res);
+                //     var id = res.data.Professor_id;
+                //     // get id and pass it
 
-                });
+                //     $location.url("/account/?type='professor'&id="+id);
+                //     }
+                // }, function(res){
+                //     console.log(res);
+
+                // });
             } else {
-                $http.post('https://vast-earth-4742.herokuapp.com/student_sign_in', {
-                    "Email": $scope.email,
-                    "Password": $scope.password
-                }).then(function(res){
-                    console.log(res);
-                    if (res.data.success == false) {
-                        alert("Invalid credentials");
-                    }
-                    else {
-                    console.log(res);
-                    var id = res.data.Student_id;
-                    // get id and pass it
+                // $http.post('https://vast-earth-4742.herokuapp.com/student_sign_in', {
+                //     "Email": $scope.email,
+                //     "Password": $scope.password
+                // }).then(function(res){
+                //     console.log(res);
+                //     if (res.data.success == false) {
+                //         alert("Invalid credentials");
+                //     }
+                //     else {
+                //     console.log(res);
+                //     var id = res.data.Student_id;
+                //     // get id and pass it
 
-                    $location.url("/mylectures/?type='student'&id="+id);
-                    }
-                }, function(res){
-                    console.log(res);
+                //     $location.url("/mylectures/?type='student'&id="+id);
+                //     }
+                // }, function(res){
+                //     console.log(res);
 
-                });
+                // });
             }
         }
     }]);
